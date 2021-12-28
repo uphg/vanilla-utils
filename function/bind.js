fb2e03bc45688249b19e2cb24863a409a1cf69c9
chen <1834805770@qq.com>
2021-12-28 22:25:24 +0800

    创建 promise 文件

--- function/bind.js
+++ function/bind.js
@@ -1,7 +1,7 @@
 // Function.prototype.bind 实现
 function bind(atThis, ...args) {
   const func = this
-  function result(...fnArgs) {
+  const result = function (...fnArgs) {
     const self = this instanceof result ? this : atThis
     return func.call(self, ...args, ...fnArgs)
   }
