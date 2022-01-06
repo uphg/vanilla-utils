18a9e24d59847f9db996411803abc6062e0d5d14
uphg <2633565885@qq.com>
2022-01-06 16:45:05 +0800

    给节流函数添加单元测试

--- function/throttle.test.js
+++ function/throttle.test.js
@@ -5,4 +5,17 @@ describe('throttle', () => {
   it('是一个函数', () => {
     assert.isFunction(throttle)
   })
+
+  it('节流可用', (done) => {
+    let counts = 0
+    const fn = () => { counts += 1 }
+    const throttleFn = throttle(fn, 32)
+
+    throttleFn(); throttleFn()
+    assert.strictEqual(counts, 1)
+    setTimeout(() => {
+      assert.strictEqual(counts, 2)
+      done()
+    }, 64)
+  })
 })
