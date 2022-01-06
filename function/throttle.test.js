026c45a9fd8dcdf180286bf827b3d2894e7f1faf
uphg <2633565885@qq.com>
2022-01-06 17:05:07 +0800

    给 throttle 函数添加一些测试用例

--- function/throttle.test.js
+++ function/throttle.test.js
@@ -9,12 +9,42 @@ describe('throttle', () => {
   it('节流可用', (done) => {
     let counts = 0
     const fn = () => { counts += 1 }
-    const throttleFn = throttle(fn, 32)
+    const throttledFn = throttle(fn, 32)
 
-    throttleFn(); throttleFn()
+    throttledFn(); throttledFn()
     assert.strictEqual(counts, 1)
     setTimeout(() => {
       assert.strictEqual(counts, 2)
+      done()
+    }, 64)
+  })
+
+  it('可传入参数', (done) => {
+    let value = null
+
+    const fn = (val) => { value = val }
+
+    const throttledFn = throttle(fn, 32)
+    throttledFn(1); throttledFn(2)
+
+    assert.strictEqual(value, 1)
+
+    setTimeout(() => {
+      assert.strictEqual(value, 2)
+      done()
+    }, 64)
+  })
+
+  it('调用一次', (done) => {
+    let counts = 0
+    const fn = () => counts += 1
+
+    const throttledFn = throttle(fn, 32)
+
+    const result = throttledFn()
+    setTimeout(() => {
+      assert.strictEqual(counts, 1, 'fn 函数被调用过一次')
+      assert.strictEqual(result, 1, 'throttled 函数会返回原始函数的返回值')
       done()
     }, 64)
   })
