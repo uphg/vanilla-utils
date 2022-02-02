9fac15b6c948d8343bc68a9ee7d339222f8ab6f6
chen <1834805770@qq.com>
2022-02-02 22:29:04 +0800

    优化 Promise 的代码

--- src/promise.ts
+++ src/promise.ts
@@ -14,41 +14,11 @@ class Promise2 {
   }
 
   resolve(result) {
-    if (this.state !== 'pending') return
-    this.state = 'fulfilled'
-    nextTick(() => {
-      this.callbacks.forEach((handle) => {
-        const func = handle[0]
-        if (isFunction(func)) {
-          let returnValue
-          try {
-            returnValue = func.call(void 0, result)
-          } catch(error) {
-            return handle[2].reject(error)
-          }
-          handle[2].resolveWith(returnValue)
-        }
-      })
-    })
+    this.resolveOrReject('fulfilled', result, 0)
   }
 
   reject(reason) {
-    if (this.state !== 'pending') return
-    this.state = 'rejected'
-    nextTick(() => {
-      this.callbacks.forEach((handle) => {
-        const func = handle[1]
-        if (typeof func === 'function') {
-          let returnValue
-          try {
-            returnValue = func.call(void 0, reason)
-          } catch(error) {
-            return handle[2].reject(error)
-          }
-          handle[2].resolveWith(returnValue)
-        }
-      })
-    })
+    this.resolveOrReject('rejected', reason, 1)
   }
 
   then(successd?, fail?) {
@@ -65,47 +35,71 @@ class Promise2 {
     return handle[2]
   }
 
-  resolveWith(param) {
-    // 如果当前 Promise (this) 等于 param，则抛出一个 TypeError 错误
-    if (this === param) {
-      // ...
-      this.reject(new TypeError(`${param} and Promise2 are not allowed to be the same reference object`))
-    }
-    // 如果 param 是一个 Promise ，采用 Promise 的状态
-    // 1. 如果是请求状态(pending)，必须保持 pending 直到状态改变
-    // 2. 如果是完成态(fulfilled)，就使用相同的值(result)来 fufill Promise
-    // 2. 如果是拒绝态(rejected)，就用相同的原因(reason)来 reject Promise
-    if (param instanceof Promise2) {
-      // 在 then 中传入的 resolve 和 reject 会默认消除 this，所以需要重新绑定
-      param.then(this.resolve.bind(this), this.reject.bind(this))
-    }
+  private resolveOrReject(state: 'fulfilled' | 'rejected', result, index: 0 | 1) {
+    if (this.state !== 'pending') return
+    this.state = state
+    nextTick(() => {
+      this.callbacks.forEach((handle) => {
+        const func = handle[index]
+        if (isFunction(func)) {
+          let returnValue
+          try {
+            returnValue = func.call(void 0, result)
+          } catch(error) {
+            return handle[2].reject(error)
+          }
+          handle[2].resolveWith(returnValue)
+        }
+      })
+    })
+  }
 
-    // 如果 param 是一个对象或方法
-    if (isObject(param) || isFunction(param)) {
-      let then
+  private resolveWith(value) {
+    if (this === value) {
+      this.resolveWithSelf(value)
+    }
+    if (value instanceof Promise2) {
+      this.resolveWithPromise(value)
+    }
+    if (isObject(value) || isFunction(value)) {
+      this.resolveWithObject(value)
+    } else {
+      this.resolve(value)
+    }
+  }
+
+  private resolveWithSelf(value) {
+    this.reject(new TypeError(`${value} and Promise2 are not allowed to be the same reference object`))
+  }
+
+  private resolveWithPromise(value) {
+    value.then(this.resolve.bind(this), this.reject.bind(this))
+  }
+
+  private getThen(value) {
+    let then
+    try {
+      then = value.then
+    } catch(error) {
+      return this.reject(error)
+    }
+    return then
+  }
+
+  private resolveWithObject(value) {
+    const then = this.getThen(value)
+    if (isFunction(then)) {
       try {
-        then = param.then
+        value.then((y) => {
+          this.resolveWith(y)
+        }, (r) => {
+          this.reject(r)
+        })
       } catch(error) {
         this.reject(error)
       }
-      // 如果 param 是一个方法
-      if (isFunction(then)) {
-        try {
-          param.then((y) => {
-            this.resolveWith(y)
-          }, (r) => {
-            this.reject(r)
-          })
-        } catch(error) {
-          this.reject(error)
-        }
-      } else {
-        // 如果 param 不是方法 则使用 resolve 处理 param
-        this.resolve(param)
-      }
     } else {
-      // 如果 param 不是对象或方法 则使用 resolve 处理 param
-      this.resolve(param)
+      this.resolve(value)
     }
   }
 }
