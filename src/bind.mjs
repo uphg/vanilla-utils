acee6f08d231eab3723bd57d6f19c42487456f89
lvheng <2633565885@qq.com>
2023-03-30 14:30:11 +0800

    调整 bind 方法变量命名

--- src/bind.mjs
+++ src/bind.mjs
@@ -1,8 +1,8 @@
 function bind(atThis, ...args) {
   const func = this
-  const result = function (...fnArgs) {
-    const self = this instanceof result ? this : atThis
-    return func.call(self, ...args, ...fnArgs)
+  const result = function(..._args) {
+    const _this = this instanceof result ? this : atThis
+    return func.apply(_this, args.concat(_args))
   }
   result.prototype = func.prototype
   return result
